025ad755aa56fc8c45b9c1caac549748efa3686b
Sebdami <Sebdami@hotmail.fr>
2016-06-23 00:53:55 +0200

    Ajout de fonctions Draw

--- Open Dreaming/Draw.hpp
+++ Open Dreaming/Draw.hpp
@@ -6,5 +6,12 @@
 #include <stdio.h>
 #include <time.h>
 #include <string>
+#include <vector>
+#include <math.h>
+#include <iostream>
 
 void drawMaison(IplImage* img, CvPoint pos, int size_x, int size_y, CvScalar color);
+void drawPolySphere(IplImage* img, CvPoint pos, int nbPts, int size, CvScalar color);
+void drawRectangle(IplImage* img, CvPoint pt1, CvPoint pt2, CvScalar color);
+void drawCercle(IplImage* img, CvPoint center, int radius, CvScalar color);
+void drawLine(IplImage* img, CvPoint pt1, CvPoint pt2, CvScalar color);
