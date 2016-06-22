4813a5c1baf3230dd7622096ff8008aa2b0d9c44
tahrky <tahrky@gmail.com>
2016-06-22 16:27:58 +0200

    Classe Graphics fonctionnelle.
    Display qui fonctionne, on peut quitter avec escape. Plus de fuite mémoire lors de l'arrêt.
    
    Positionnement aléatoire dans la fenêtre.

--- Open Dreaming/Graphics.hpp
+++ Open Dreaming/Graphics.hpp
@@ -6,18 +6,29 @@
 #include <stdio.h>
 #include <time.h>
 #include <string>
+#include <iostream>
 
 #define CV_NO_BACKWARD_COMPATIBILITY
 
-#define NUMBER 100
+#define NUMBER 20
 #define DELAY 5
 
+struct Color
+{
+	unsigned char red, green, blue;
+};
 
 class Graphics
 {
 	private :
-		char wndname[] = "Open Dreaming";
+		char * wndname;
+		unsigned int width, height;
 
 	public : 
+		Graphics ();
+		~Graphics ();
 
+		void color ();
+
+		void display (const char *);
 };
