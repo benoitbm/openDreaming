54c7a1aaec4cf570bd61d87d54ee2afea667447b
Sebdami <Sebdami@hotmail.fr>
2016-06-22 16:11:34 +0200

    Ajout de Draw.cpp et Draw.hpp
    voilà voilà

---
+++ Open Dreaming/Draw.cpp
@@ -0,0 +1,9 @@
+#include "Draw.hpp"
+
+void Draw::drawRectangle(Mat& img, Point pos, int width, int height, CV_RGB color)
+{
+	int thickness = (rand() % 5) + 1;
+	int line_type = 8;
+	int shift = 0;
+	rectangle(img, pos, Point(pos.x, pos.y), color, thickness, line_type, shift);
+}
