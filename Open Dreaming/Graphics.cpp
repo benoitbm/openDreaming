29cd7732b9741afe6399c41f245cb50f79191c7c
Sebdami <Sebdami@hotmail.fr>
2016-06-22 18:37:27 +0200

    Ajout de la fonction drawMaison

--- Open Dreaming/Graphics.cpp
+++ Open Dreaming/Graphics.cpp
@@ -23,6 +23,15 @@ CvScalar random_color(CvRNG* rng)
 	return CV_RGB(rouge, vert, bleu);
 }
 
+void drawMaison(IplImage* img, CvPoint pos, int size_x, int size_y, CvScalar color)
+{
+	int thickness = (rand() % 5) + 1;
+	cvRectangle(img, cvPoint(pos.x - size_x, pos.y - size_y), cvPoint(pos.x + size_x, pos.y + size_y), color, thickness, 8, 0);
+	cvLine(img, cvPoint(pos.x - size_x, pos.y - size_y), cvPoint(pos.x, pos.y - (2 * size_y)), color, thickness, 8, 0);
+	cvLine(img, cvPoint(pos.x, pos.y - (2 * size_y)), cvPoint(pos.x + size_x, pos.y - size_y), color, thickness, 8, 0);
+	cvRectangle(img, cvPoint(pos.x - size_x / 4, pos.y + size_y / 3), cvPoint(pos.x + size_x / 4, pos.y + size_y), color, thickness, 8, 0);
+}
+
 void Graphics::display()
 {
 	readFile();
